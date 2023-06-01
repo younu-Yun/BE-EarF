e0b52c655f1b01f6f6644600a451255c61317120
paikpaik <woduf9311@gmail.com>
2023-06-01 12:38:00 +0900

    Init: 초기 설정 및 Feat: user, badge 스키마 추가

---
+++ models/index.ts
@@ -0,0 +1,4 @@
+import User from "./schemas/user.js";
+import Badge from "./schemas/badge.js";
+
+export { User, Badge };
---
+++ models/schemas/user.ts
@@ -0,0 +1,76 @@
+import { Schema, Document } from "mongoose";
+
+interface IUser extends Document {
+  id: string;
+  password: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  profileImage: string;
+  badges: [];
+  postNum: number;
+  tumblerNum: number;
+  transportNum: number;
+  basketNum: number;
+}
+
+const UserSchema: Schema<IUser> = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      default: "",
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    badges: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Badge",
+      },
+    ],
+    postNum: {
+      // 3회 이상 연속작성, 커뮤니티 포스팅 10회
+      type: Number,
+      default: 0,
+    },
+    tumblerNum: {
+      // 텀블러 3회 사용
+      type: Number,
+      default: 0,
+    },
+    transportNum: {
+      // 대중교통 이용 3회
+      type: Number,
+      default: 0,
+    },
+    basketNum: {
+      // 장바구니 3회 사용
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true } // 최초작성, 3회 이상 연속작성
+);
+
+export default UserSchema;
