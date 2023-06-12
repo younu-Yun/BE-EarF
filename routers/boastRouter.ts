6c344663094fc813a818ca5027272cd5a8e6e211
ChanghyeonO <ckdgus5189@gmail.com>
2023-06-12 23:10:26 +0900

    Fix: 게시글 페이지네이션 안되는 문제, 최근댓글 질문 조회 안되는 문제 수정

--- routers/boastRouter.ts
+++ routers/boastRouter.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import BoastController from "../controller/boastController";
+import authAccess from "../middlewares/authAccess";
 
 const boastRouter = Router();
-
+//자랑하기 단일 게시글 불러오기
+boastRouter.get("/boasts/:id", BoastController.loadSingleDiary);
 // 자랑하기 게시글 불러오기
 boastRouter.get("/boasts", BoastController.loadBoast);
+// 자랑하기 게시글 좋아요/ 취소
+boastRouter.post("/boasts/:id/like", authAccess, BoastController.toggleLike);
 
 export default boastRouter;
--- services/boastService.ts
+++ services/boastService.ts
@@ -1,37 +1,68 @@
-import Boast, { IBoast } from "../models/schemas/boast";
 import { Diary } from "../models/schemas/diary";
 
 const boastService = {
-  // shareStatus가 true인 다이어리 찾기
-  async loadBoast(): Promise<IBoast[]> {
+  // _id를 이용해 shareStatus가 true인 단일 다이어리 게시글 찾기
+  async loadSingleDiary(id: string) {
     try {
-      const diaries = await Diary.find({ shareStatus: true });
-
-      const boasts: IBoast[] = [];
-      for (let diary of diaries) {
-        const boast = new Boast({
-          name: diary.name,
-          profileImage: diary.profileImage,
-          checkedBadge: diary.checkedBadge,
-          tag: diary.tag,
-          title: diary.title,
-          content: diary.content,
-          likeIds: diary.likeIds,
-          createdAt: diary.createdAt,
-          imageUrl: diary.imageUrl,
-          date: diary.date,
-          diaryId: diary._id,
-        });
-        await boast.save();
-        boasts.push(boast);
+      const diary = await Diary.findOne({ _id: id, shareStatus: true });
+      if (!diary) {
+        throw new Error(
+          "해당 ID를 가진 게시글이 존재하지 않거나 공유 상태가 아닙니다.",
+        );
       }
+      return diary;
+    } catch (error) {
+      console.error(error);
+      throw new Error("다이어리 게시글을 불러오는데 실패했습니다.");
+    }
+  },
 
-      boasts.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
-
-      return boasts;
+  // shareStatus가 true인 다이어리 찾기
+  async loadBoast() {
+    try {
+      const diaries = await Diary.find({ shareStatus: true }).sort({
+        createdAt: -1,
+      });
+      return diaries;
     } catch (error) {
       console.error(error);
       throw new Error("자랑하기 게시글을 불러오는데 실패했습니다.");
+    }
+  },
+
+  //tag 검색 기능, tag.length가 1 이하인 게시글만 검색
+  async searchByTag(tag: string) {
+    try {
+      const diaries = await Diary.find({
+        shareStatus: true,
+        tag: { $in: [tag] },
+      }).sort({ createdAt: -1 });
+      return diaries;
+    } catch (error) {
+      console.error(error);
+      throw new Error("태그 검색에 실패했습니다.");
+    }
+  },
+
+  // 다이어리 게시글 좋아요 누르기 / 취소하기
+  async toggleLike(diaryId: string, userId: string) {
+    try {
+      const diary = await Diary.findById(diaryId);
+      if (!diary) {
+        throw new Error("게시글을 찾을 수 없습니다.");
+      }
+
+      const likeIndex = diary.likeIds.indexOf(userId);
+      if (likeIndex === -1) {
+        diary.likeIds.push(userId);
+      } else {
+        diary.likeIds.splice(likeIndex, 1);
+      }
+      await diary.save();
+      return diary;
+    } catch (error) {
+      console.error(error);
+      throw new Error("다이어리 게시글 좋아요 기능 처리에 실패하였습니다.");
     }
   },
 };
