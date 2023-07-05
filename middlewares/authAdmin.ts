6f7f2ab36add537f16f36be0011ebd2e1e5eedf9
paikpaik <woduf9311@gmail.com>
2023-07-05 17:43:35 +0900

    Feat: 관리자 미들웨어 추가

---
+++ middlewares/authAdmin.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express";
+import { IUser, User } from "../models/index";
+
+const authAdmin = async (req: Request, res: Response, next: NextFunction) => {
+  const { _id } = req.user as IUser;
+  const user = await User.findOne({ _id });
+
+  if (!user || !user.isAdmin) {
+    res.status(400).json({
+      message:
+        "관리자 접근 권한이 필요합니다. 관리자 계정으로 다시 로그인해주세요",
+    });
+  } else {
+    next();
+  }
+};
+
+export default authAdmin;
